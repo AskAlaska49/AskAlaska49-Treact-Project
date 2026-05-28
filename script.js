31c284411bfc23993c147a4325bc29c598eb447b
Barbara Ralston <barbararalston@Barbaras-MacBook-Pro.local>
2026-05-28 02:38:57 -0800

    added JS and @keyframes and now the cards are gone. They need to come in from the right and stay.

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove("menu--open");
 }
+
+const plans = document.querySelectorAll(".plan");
+
+function revealPlans() {
+  plans.forEach((plan) => {
+    const revealTop = plan.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (revealTop < windowHeight - 100) {
+      plan.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealPlans);
+revealPlans();
