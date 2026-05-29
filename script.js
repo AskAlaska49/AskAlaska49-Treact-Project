c3db641a7ca260bda9e17b5c172643b21a3d76e9
Barbara Ralston <barbararalston@Barbaras-MacBook-Pro.local>
2026-05-28 16:54:16 -0800

    Changed the script.js, corrected the cards, now they slideinright.

--- script.js
+++ script.js
@@ -9,15 +9,18 @@ function closeMenu() {
 const plans = document.querySelectorAll(".plan");
 
 function revealPlans() {
-  plans.forEach((plan) => {
-    const revealTop = plan.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+plans.forEach((plan) => {
+const revealTop = plan.getBoundingClientRect().top;
+const revealBottom = plan.getBoundingClientRect().bottom;
+const windowHeight = window.innerHeight;
 
-    if (revealTop < windowHeight - 100) {
-      plan.classList.add("show");
-    }
-  });
+if (revealTop < windowHeight - 100 && revealBottom > 0) {
+plan.classList.add("show");
+} else {
+plan.classList.remove("show");
+}
+});
 }
 
 window.addEventListener("scroll", revealPlans);
-revealPlans();
+window.addEventListener("load", revealPlans);
